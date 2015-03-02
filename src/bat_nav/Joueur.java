3c1c54e2ec7d943deff3110112925daa9493b339
edshu <edouard.shu@ecl13.ec-lyon.fr>
2015-03-02 09:54:57 +0100

    class joueur

--- src/bat_nav/Joueur.java
+++ src/bat_nav/Joueur.java
@@ -1,10 +1,50 @@
 package bat_nav;
 
-public class Joueur {
-	//Contient le score
+import java.awt.FlowLayout;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class Joueur extends JFrame{
+	//Contient le score, historique, les bateaux qu'il reste
 	private String name;
+	private JLabel label;
+	private JTextField t;
 	
-	public void setName(String inputName){
+	private JPanel buildContentPane(){
+		JPanel IN = new JPanel();
+		IN.setLayout(new FlowLayout());
 		
+		t = new JTextField();
+		
+		label = new JLabel("Rien pour le moment");
+		
+		return IN;
 	}
+	
+	public JTextField getTextField(){
+		return t;
+	}
+ 
+	public JLabel getLabel(){
+		return label;
+	}
+
+}
+
+
+public class GetAction extends AbstractAction {
+	private FenetreSaisie fenetre;
+ 
+	public GetAction(FenetreSaisie fenetre, String texte){
+		super(texte);
+ 
+		this.fenetre = fenetre;
+	}
+ 
+	public void actionPerformed(ActionEvent e) { 
+		String texteUtilisateur = fenetre.getTextField().getText();
+		fenetre.getLabel().setText(texteUtilisateur);
+	} 
 }
