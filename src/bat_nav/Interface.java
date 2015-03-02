cb9221c7032ce1afe87358ede0486f1181680d94
Aymeric <aymeric.agre@gmail.com>
2015-03-02 09:37:15 +0100

    lol

--- src/bat_nav/Interface.java
+++ src/bat_nav/Interface.java
@@ -1,10 +1,22 @@
 package bat_nav;
 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 public class Interface {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		JFrame f = new JFrame("Bataille Navale");
+		JPanel p = (JPanel)f.getContentPane();
+		JPanel damier = new JPanel();
+		JPanel infoJoueur = new JPanel(); 
+		
+		p.add("West", damier);
+		p.add("East", infoJoueur);
+		f.pack();
+		f.setSize(1000, 600);
+		f.setVisible(true);
 	}
 	
 }
