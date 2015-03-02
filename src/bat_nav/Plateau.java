b2ed8b59df499576de176d001e45e7c1447b00d6
DorianWilde <adrien.sorgniard@ecl13.ec-lyon.fr>
2015-03-02 09:55:50 +0100

    tien le plateau

--- src/bat_nav/Plateau.java
+++ src/bat_nav/Plateau.java
@@ -4,5 +4,40 @@ public class Plateau {
 	//Contient le placement des bateaux
 	//Contient les coups joués précédemment
 
+	int[][] plateau;
+	int[][] viseur;
+	int taille;
+	int absShot;
+	int ordShot;
+	
+	Plateau(int t)
+	{
+		taille=t;
+		plateau = new int[taille][taille];
+		for(int i=0; i<taille; i++)
+		{
+			for(int j=0; j<taille; j++)
+			{
+				plateau[i][j] = 0;
+				viseur[i][j] = 0;
+			}
+		}
+	}
+	
+	public int coupJoue(int a, int b)
+	{
+		return plateau[a][b];
+	}
+	
+	public void resultatTir(int a)
+	{
+		viseur[absShot][ordShot] = a;
+	}
+	
+	public void cible (int a, int b)
+	{
+		absShot = a;
+		ordShot = b;
+	}
 
 }
