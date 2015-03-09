29dd0a5147813cfb9e054e4e5e509e95415e772c
adong <anthony.dong@ecl13.ec-lyon.fr>
2015-03-09 10:59:00 +0100

    Reseau

--- src/bat_nav/Reseau.java
+++ src/bat_nav/Reseau.java
@@ -5,19 +5,24 @@ import java.util.Random;
 public class Reseau {
 	
 	Plateau plateau;
+	//Plateau/Socket joueur_distant;
 	int joueur;
 	int premier_joueur;
 	
 	//on essaye de se connecter à telle personne
 	//client = joueur 1
 	//serveur = joueur 2
+	//il faut se mettre d'accord
 	
 	public Reseau(Plateau p) {
 		plateau = p;
+		//connecter au joueur distant ici
+		//determiner le numero des joueurs ici + leurs noms
 		Random rand = new Random();
 		premier_joueur = rand.nextInt(2)+1;
 	}
 	
+	//appelé par nous quand on veut jouer un coup sur la grille du joueur distant
 	public int joueCoup(int x, int y) {
 		//le joueur joue un coup à telle coordonnée,
 		//on transmet sur le réseau
@@ -25,18 +30,16 @@ public class Reseau {
 		//0: plouf
 		//1: touché
 		//2: coulé
+		int resultat = 0;
 		
 		//ecrire sur le socket
-		int resultat = ...(x, y);
-		//p.joueCoupSurSaGrille(x, y); // c'est fait par l'interface?
+		//resultat = joueur_distant.recoitCoup(x, y);
 		return resultat;
 	}
 	
-	//socket callback
-	public void recoitCoup() {
-		//read le socket pour définir x,y
-		int x,y;
-		int resultat = p.joueCoupSurMaGrille(x, y);
-		//socket pour renvoyer resultat
+	//appelé par le joueur distant quand il veut jouer un coup sur notre grille
+	public int recoitCoup(int x, int y) {
+		int resultat = plateau.coupJoue(x, y);
+		return resultat;
 	}
 }
