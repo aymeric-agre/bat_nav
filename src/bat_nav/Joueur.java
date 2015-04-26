8391810941c13b2772b6d07ab5e36c839d97fdd4
Aymeric <aymeric.agre@gmail.com>
2015-04-26 19:06:36 +0200

    Changement correct joueur

--- src/bat_nav/Joueur.java
+++ src/bat_nav/Joueur.java
@@ -9,20 +9,22 @@ public class Joueur extends JPanel {
 	//Contient le score, historique, les bateaux qu'il reste
 	private String name;
 	private Integer score;
+	private JLabel nomJoueur;
 	
 	Joueur(String newName){
+		this.nomJoueur = new JLabel(this.name);
+		add("North", nomJoueur);
 		changePlayer(newName);
 	}
 	
 	public void changePlayer(String newName){
 		this.name = newName;
 		this.score = 0;
+		this.nomJoueur.setText(newName);
 		System.out.println("Création d\'un nouveau joueur: " + newName);
 	}
 
 	public void paintComponent(Graphics g){
-		JLabel nomJoueur = new JLabel(this.name);
-		add("North", nomJoueur);
 	}
 }
 
